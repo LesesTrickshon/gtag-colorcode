63be609f925b3d25bca35ecd5f8cf486a483de60
LT :3 <88660663+LesesTrickshon@users.noreply.github.com>
2024-12-31 18:51:31 +0100

    Added comments to colorcode.py

--- colorcode.py
+++ colorcode.py
@@ -9,6 +9,7 @@ Mode 3: Convert HEX to RGB
 
       """)
 
+# Asks which mode to use
 mode = int(input("Mode: "))
 if mode == 1 or mode == 2:
     color[0] = int(input("Red Value: "))
@@ -19,12 +20,14 @@ elif mode == 3:
 
 def RgbToMonke():
     global check
+      # Checks if value is valid
     for i in range(3):
         if color[i] > 255:
             check = False
             print("Each Color Value can be 255 at max.")
             break
 
+      # Converts by using the rule of three method
     if check:
         for i in range(3):
             color[3] = color[i] / 255
@@ -32,12 +35,13 @@ def RgbToMonke():
 
 def MonkeToRbg():
     global check
+      # Checks if value is valid
     for i in range(3):
         if color[i] > 9:
             check = False
             print("Each Color Value can be 9 at max.")
             break
-
+      # Converts by using the rule of three method
     if check:
         for i in range(3):
             color[3] = color[i] / 9
@@ -45,6 +49,7 @@ def MonkeToRbg():
 
 def HexToRgb():
     global r, g, b, hex_code
+      #Had to use chatgpt cause wtf is this shit
     hex_code = hex_code.lstrip('#')
     color[0] = int(hex_code[0:2], 16)
     color[1] = int(hex_code[2:4], 16)
